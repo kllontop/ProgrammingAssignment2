cd935316fa31fa75f7dfda0e5559ce4f114cbc8c
kllontop <kb.llontop@gmail.com>
2014-05-21 14:30:18 -0400

    example scripts for ProgrammingAssignment 2

---
+++ cachemean.R
@@ -0,0 +1,12 @@
+##Example for Programming Assignment 2: Lexical Scoping
+cachemean <- function(x, ...) {
+        m <- x$getmean()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- mean(data, ...)
+        x$setmean(m)
+        m
+}
